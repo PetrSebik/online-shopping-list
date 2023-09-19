b083cba533f95d905c22f7cdb653483bb66fbfb9
Petr Sebik <sebikpetr@gmail.com>
2023-09-20 00:27:31 +0200

    redirect

--- conf/urls.py
+++ conf/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name='shopping_list', permanent=True)),
     path("admin/", admin.site.urls),
     path("shopping/", include('apps.shopping.urls'))
 ]
