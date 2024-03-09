89cd79c45c4b3858623acd6f661bf1e266a144f9
Petr Sebik <sebikpetr@gmail.com>
2024-03-09 16:02:53 +0100

    disabled debug-bar and added admin for recipes

--- apps/recipe/admin.py
+++ apps/recipe/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Recipe, RecipeItem, RecipeStep
 
-# Register your models here.
+
+class RecipeItemInline(admin.TabularInline):
+    model = RecipeItem
+    extra = 1
+
+
+class RecipeStepInline(admin.TabularInline):
+    model = RecipeStep
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'last_cooked_date')
+    inlines = [RecipeItemInline, RecipeStepInline]
+
+
+@admin.register(RecipeItem)
+class RecipeItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'recipe', 'count', 'units', 'description')
+
+
+@admin.register(RecipeStep)
+class RecipeStepAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'number', 'text')
--- conf/settings.py
+++ conf/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
+    # "debug_toolbar",
     "apps.shopping",
     "apps.recipe",
 ]
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "conf.urls"
