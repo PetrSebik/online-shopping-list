ea279b6580c8025d1558cbf961d259cc301aefc8
petrsebik <petr.sebik@skoumal.net>
2025-11-27 22:52:29 +0100

    feature(recipe): added filtering

--- apps/recipe/urls.py
+++ apps/recipe/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from .views import (
     RecipesListView,
+    RecipesListFilteredView,
     RecipeDetailView,
     RecipeCreateView,
 )
 
 urlpatterns = [
     path('list/', RecipesListView.as_view(), name='recipes_list'),
+    path('list/filtered/', RecipesListFilteredView.as_view(template_name='recipes_list_partial.html'),
+         name='recipes_list_filtered'),
     path('detail/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
     path('create/', RecipeCreateView.as_view(), name='recipe_create'),
 ]
--- apps/recipe/views.py
+++ apps/recipe/views.py
@@ -2,13 +2,61 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 
 from .forms import CreateRecipeForm, RecipeItemFormSet, RecipeStepFormSet
-from .models import Recipe
+from .models import Recipe, Tag
 
 
 class RecipesListView(ListView):
     template_name = 'recipes_list.html'
     queryset = Recipe.objects.prefetch_related('tags').all()
     context_object_name = 'recipes'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        active_tag_ids = set()
+        all_tags = Tag.objects.all().order_by("name")
+        for t in all_tags:
+            t.is_active = True
+            active_tag_ids.add(t.id)
+
+        ctx["all_tags"] = all_tags
+        ctx["active_tag_ids"] = active_tag_ids
+        return ctx
+
+
+class RecipesListFilteredView(ListView):
+    template_name = 'recipes_list_partial.html'
+    queryset = Recipe.objects.prefetch_related('tags').all()
+    context_object_name = 'recipes'
+
+    def get_tag_ids(self) -> list[int]:
+        """
+        Parse 'tags' GET parameter into a list of integers.
+        Ignores empty strings or invalid integers.
+        """
+        tags_param = self.request.GET.get('tags', '')
+        return [int(t.strip()) for t in tags_param.split(',') if t.strip().isdigit()]
+
+    def get_queryset(self):
+        qs = Recipe.objects.prefetch_related('tags')
+        tag_ids = self.get_tag_ids()
+        if tag_ids:
+            qs = qs.filter(tags__in=tag_ids).distinct()
+        elif len(tag_ids) == 0:
+            qs = qs.none()
+        return qs
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        active_tag_ids = self.get_tag_ids()
+        all_tags = Tag.objects.all().order_by("name")
+        for t in all_tags:
+            t.is_active = t.id in active_tag_ids
+
+        ctx["all_tags"] = all_tags
+        ctx["active_tag_ids"] = active_tag_ids
+        return ctx
 
 
 class RecipeDetailView(DetailView):
@@ -32,7 +80,6 @@ class RecipeCreateView(CreateView):
         item_formset = RecipeItemFormSet(self.request.POST, instance=recipe)
         step_formset = RecipeStepFormSet(self.request.POST, instance=recipe)
 
-        print(item_formset, step_formset)
         if item_formset.is_valid() and step_formset.is_valid():
             item_formset.save()
             step_formset.save()
