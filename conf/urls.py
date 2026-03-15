d5e7fee85de4a8ebf4eb48f051a400c4815136ad
petrsebik <petr.sebik@skoumal.net>
2026-03-15 17:12:20 +0100

    fix(core): fixed issue with same name on template

--- conf/urls.py
+++ conf/urls.py
@@ -17,12 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", RedirectView.as_view(pattern_name="htmx_shopping", permanent=True)),
     path("", include("apps.core.urls")),
     path("admin/", admin.site.urls),
     path("shopping/", include("apps.shopping.urls")),
